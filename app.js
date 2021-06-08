7dfdb911989b6d84e85134b5ccdf5fec34ecea9d
Julie Barwick <julie@juliebarwick.com>
2021-06-08 07:47:36 -0700

    Further develop shuffled number list and add a default square when doc ready

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var $toneSquare = $('#tr-square');
 
 $(document).ready(function() {
 
+  drawSquares(12);
+
   $('#draw').on('click', function() {
     var n = $('input').val();
     drawSquares(n);
+    createShuffledNumList(n);
   })
 
 
@@ -35,15 +38,22 @@ var drawSquares = function(n) {
 }
 
 
-var shuffle = function(arr) {
-  var res = arr.slice();
-  for (var i = res.length - 1; i > 0; i--) {
-    var randomNum = Math.floor(Math.random() * i);
-    var temp = res[i];
-    res[i] = res[randomNum];
-    res[randomNum] = temp
+var createShuffledNumList = function(n) {
+  var res = [];
+
+  for (var i = 0; i < n; i++) {
+    res.push(i);
   }
 
+  for (var j = res.length - 1; j > 0; j--) {
+    // Get a random index number, from current current index to
+    var randomNum = Math.floor(Math.random() * j);
+    // Swap current element with randomly selected element
+    var temp = res[j];
+    res[j] = res[randomNum];
+    res[randomNum] = temp
+  }
+  console.log(res)
   return res;
 }
 
