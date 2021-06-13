71a6593134f153ba5b4ec4c82a0521fd928b27fa
Julie Barwick <julie@juliebarwick.com>
2021-06-13 11:51:44 -0700

    Build most functionality to draw square, change display modes, etc.

--- app.js
+++ app.js
@@ -1,28 +1,119 @@
 var $toneSquare = $('#tr-square');
+var toneSquare = [];
+var mode = 'numeric';
+var rowLength = 12;
+var clicked = 0;
 
 $(document).ready(function() {
 
   // Default view
-  drawSquares();
+  drawSquares(rowLength);
+  drawPitchButtons();
+  $('#numeric').addClass('highlighted');
 
   $('#draw').on('click', function() {
-    var n = Number($('input').val());
+    clearAll();
 
     // Check/force input to be a whole number
-    var res = checkInput(n);
+    var res = checkInput(Number($('input').val()));
     if (!res) {
       return;
     } else {
-      n = res;
+      rowLength = res;
     }
 
-    drawSquares(n);
-    var originalRow = createShuffledNumList(n);
-    var squareArr = calcSquareValues(originalRow);
-    fillSquares(squareArr)
+    drawSquares(rowLength);
+  });
+
+  $('#random').on('click', function() {
+    createAndFillSquare(rowLength, mode);
+    $('#user-row').html('')
+    var row = toneSquare[0];
+    for (var i = 0; i < row.length; i++) {
+      addRowText(row[i]);
+    }
   })
 
+  $('.mode').on('click', function() {
+    $('button.mode').removeClass('highlighted');
+    $(this).addClass('highlighted');
+    mode = $(this).attr('id');
+
+    var $allPitchInputs = $('.pitch-input');
+
+    $allPitchInputs.each(function(index, element) {
+      var input = index;
+      if (mode !== 'numeric') {
+        input = convertToLetter(input, mode);
+      }
+      $(this).html(input);
+    });
+
+    if (toneSquare.length === rowLength) {
+      var inputSquare = toneSquare;
+      if (mode !== 'numeric') {
+        inputSquare = convertSquareToLetters(toneSquare, mode);
+      }
+      fillSquares(inputSquare);
+    }
+
+    var $rowSpans = $('.original-row-item');
+    if ($rowSpans) {
+      $rowSpans.each(function(index, element) {
+        var input = index;
+        if (mode !== 'numeric') {
+          input = convertToLetter(input, mode);
+        }
+        $(this).html(input);
+      })
+    }
+  });
+
+  $('#randomize').on('click', function() {
+    var randomLength = Math.floor(Math.random() * 12) + 1;
+    createAndFillSquare(randomLength, mode);
+    rowLength = randomLength;
+    $('#user-row').html('');
+    makeAllUnclicked();
+    var row = toneSquare[0];
+    for (var i = 0; i < row.length; i++) {
+      addRowText(row[i]);
+    }
+  });
+
+  $('.pitch-input').on('click', function() {
+    if (!clicked) {
+      clearAll();
+    }
+    clicked++;
+    var n = Number($(this).attr('id'));
+    addRowText(n);
+
+    toneSquare.push(n);
+    $(this).addClass('clicked');
+
+    if (rowLength === clicked) {
+      toneSquare = calcSquareValues(toneSquare)
+      fillSquares(toneSquare);
+      makeAllClicked();
+    }
+  });
+
+  $('#clear').on('click', clearAll)
 });
+
+var createAndFillSquare = function(n, mode) {
+  drawSquares(n);
+  var originalRow = createShuffledNumList(n);
+  toneSquare = calcSquareValues(originalRow);
+
+  if (mode !== 'numeric') {
+    var letters = convertSquareToLetters(toneSquare, mode);
+    fillSquares(letters);
+  } else {
+    fillSquares(toneSquare);
+  }
+}
 
 var drawSquares = function(n) {
 
@@ -45,11 +136,42 @@ var drawSquares = function(n) {
 var fillSquares = function(arr) {
 
   $('.tr').each(function(i, row) {
-
     $('.pitch-box', this).each(function(j, box) {
-      $(this).text(arr[i][j]);
+      $(this).html(arr[i][j]);
     });
   });
+}
+
+var convertSquareToLetters = function(arr, pref) {
+
+  // Make a copy of the nested array
+  var squareInLetters = arr.map(function(a) {
+    return a.slice();
+  });
+
+  for (var i = 0; i < squareInLetters.length; i++) {
+    for (var j = 0; j < squareInLetters[i].length; j++) {
+      squareInLetters[i][j] = convertToLetter(squareInLetters[i][j], pref);
+    }
+  }
+  return squareInLetters;
+}
+
+var convertToLetter = function(num, pref) {
+
+  var letters = {
+    0: 'C',
+    2: 'D',
+    4: 'E',
+    5: 'F',
+    7: 'G',
+    9: 'A',
+    11: 'B'
+  };
+  if (pref === 'sharp') {
+    return letters[num] ? letters[num] : letters[num - 1] + '&#9839;'
+  }
+  return letters[num] ? letters[num] : letters[num + 1] + '&#9837'
 }
 
 var generateListOfPitches = function(n) {
@@ -122,14 +244,36 @@ var calcSquareValues = function(oRow) {
   return square;
 }
 
-function prettyPrintRow(square) {
-  for (var i = 0; i < square.length; i++) {
-    var strRow = [];
-    for (var j = 0; j < square[i].length; j++) {
-      var str = square[i][j].toString();
-      strRow.push(str.padStart(2, ' '));
-    }
-    console.log(strRow.join("\t"));
+var drawPitchButtons = function(pref) {
+  $pitchInputArea = $('#pitch-select');
+  for (var i = 0; i < 12; i++) {
+    $pitchButton = $('<div>').addClass('pitch-input').attr('id', i)
+    $pitchButton.text(i)
+    $pitchInputArea.append($pitchButton);
   }
 }
 
+var clearAll = function() {
+  $('.pitch-box').html('');
+  toneSquare = [];
+  makeAllUnclicked();
+  $('#user-row').html('');
+}
+
+var makeAllClicked = function() {
+  $('.pitch-input').addClass('clicked');
+  clicked = 12;
+}
+
+var makeAllUnclicked = function() {
+  $('.pitch-input').removeClass('clicked');
+  clicked = 0;
+}
+
+var addRowText = function(str) {
+  if (mode !== 'numeric') {
+    str = convertToLetter(str);
+  }
+  var $rowItem = $('<span>').addClass('original-row-item').html(str)
+  $('#user-row').append($rowItem);
+}
